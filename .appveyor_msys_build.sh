54ea8daa0b563daef79c3d9f2431fd69c638b0cd
William Hart <goodwillhart@googlemail.com>
2017-09-05 01:21:35 +0100

    999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999

--- .appveyor_msys_build.sh
+++ .appveyor_msys_build.sh
@@ -6,6 +6,6 @@ if [ ! -d "/c/projects/flint2/mpir-2.7.0" ]; then
     ./.build_dependencies
 fi
 
-./configure ABI=$ABI --with-mpir=mpir-2.7.0 --with-mpfr=mpfr-3.1.4 --disable-shared
+./configure ABI=$ABI --with-mpir=mpir-2.7.0 --with-mpfr=mpfr-3.1.5 --disable-shared
 make -j4
 make -j4 check
